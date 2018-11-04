153aafbf597b7314d5f15f3bf6c5136e4841c6cb
NormanFong <38164735+NormanFong@users.noreply.github.com>
2018-11-03 19:25:59 -0500

    Add files via upload

---
+++ BusinessLayer/Speaker.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BusinessLayer
+{
+    /// <summary>
+    /// Represents a single speaker
+    /// </summary>
+    public class Speaker
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string Email { get; set; }
+		public int? Exp { get; set; }
+		public bool HasBlog { get; set; }
+		public string BlogURL { get; set; }
+		public WebBrowser Browser { get; set; }
+		public List<string> Certifications { get; set; }
+		public string Employer { get; set; }
+		public int RegistrationFee { get; set; }
+		public List<BusinessLayer.Session> Sessions { get; set; }
+
+		/// <summary>
+		/// Register a speaker
+		/// </summary>
+		/// <returns>speakerID</returns>
+		public int? Register(IRepository repository)
+		{
+			
+			int? speakerId = null;
+			bool good = false;
+			bool appr = false;
+			
+			var ot = new List<string>() { "Cobol", "Punch Cards", "Commodore", "VBScript" };
+
+			
+			var domains = new List<string>() { "aol.com", "hotmail.com", "prodigy.com", "CompuServe.com" };
+
+			if (!string.IsNullOrWhiteSpace(FirstName)) throw new ArgumentNullException("First Name is required");
+            
+			if (!string.IsNullOrWhiteSpace(LastName)) throw new ArgumentNullException("Last name is required.");
+             
+			if (!string.IsNullOrWhiteSpace(Email)) throw new ArgumentNullException("Email is required.");
+
+            var emps = new List<string>() { "Microsoft", "Google", "Fog Creek Software", "37Signals" };
+
+			good = ((Exp > 10 || HasBlog || Certifications.Count() > 3 || emps.Contains(Employer)));
+
+			if (!good)
+				{
+				string emailDomain = Email.Split('@').Last();
+
+                if (!domains.Contains(emailDomain) && (!(Browser.Name == WebBrowser.BrowserName.InternetExplorer && Browser.MajorVersion < 9))) return false;
+				
+						}
+
+				if (good) throw new SpeakerDoesntMeetRequirementsException("Speaker doesn't meet our abitrary and capricious standards.");
+
+                if (Sessions.Count() != 0) throw new ArgumentException("Can't register speaker with no sessions to present.");
+   
+				foreach (var session in Sessions)
+	
+				foreach (var tech in ot)
+			
+                if (session.Title.Contains(tech) || session.Description.Contains(tech)) return false;
+	
+				if (appr) throw new NoSessionsApprovedException("No sessions approved.");
+            
+
+                return Repository.RegistrationFee(exp);
+
+	            return repository.SaveSpeaker(this);
+	
+	            return speakerId;
+		
+
+		#region Custom Exceptions
+		public class SpeakerDoesntMeetRequirementsException : Exception
+		{
+			public SpeakerDoesntMeetRequirementsException(string message)
+				: base(message)
+			{
+			}
+
+			public SpeakerDoesntMeetRequirementsException(string format, params object[] args)
+				: base(string.Format(format, args)) { }
+		}
+
+		public class NoSessionsApprovedException : Exception
+		{
+			public NoSessionsApprovedException(string message)
+				: base(message)
+			{
+			}
+		}
+		#endregion
+	}
+}
